1ec7cd3b04ae1a0e6f7313e31c83a1667a1f9c75
lqian2 <light.qian@infor.com>
2018-02-02 15:16:45 +0800

    Add CommonUtility

---
+++ CommonUtility/Utility/CommandLine.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utility.Cmd
+{
+    public class CommandLine
+    {
+        public static string ExecuteCommandSync(string workingDir, string command, bool showCmdWindow)
+        {
+            string result = string.Empty;
+            try
+            {
+                // create the ProcessStartInfo using "cmd" as the program to be run,
+                // and "/c " as the parameters.
+                // Incidentally, /c tells cmd that we want it to execute the command that follows,
+                // and then exit.
+                System.Diagnostics.ProcessStartInfo procStartInfo =
+                    new System.Diagnostics.ProcessStartInfo("cmd", "/c " + command);
+                procStartInfo.WorkingDirectory = workingDir;
+                // The following commands are needed to redirect the standard output.
+                // This means that it will be redirected to the Process.StandardOutput StreamReader.
+                procStartInfo.RedirectStandardOutput = true;
+
+                procStartInfo.UseShellExecute = false;
+                // Do not create the black window.
+                procStartInfo.CreateNoWindow = !showCmdWindow;
+                
+                // Now we create a process, assign its ProcessStartInfo and start it
+                System.Diagnostics.Process proc = new System.Diagnostics.Process();
+                proc.StartInfo = procStartInfo;
+                proc.Start();
+                // Get the output into a string
+                result = proc.StandardOutput.ReadToEnd();
+            }
+            catch (Exception objException)
+            {
+                result = objException.Message;
+            }
+            return result;
+        }
+    }
+}
---
+++ CommonUtility/Utility/FileUtility.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utility.FileUtil
+{
+    public class FileUtility
+    {
+        #region CreateFolder
+        public static void CreateFolder(string path)
+        {
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+        }
+        #endregion
+
+        #region GetFileList
+        public static string [] GetFileList(string directory, string searchPattern, bool searchSubFolders)
+        {
+            SearchOption searchOption = searchSubFolders ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly;
+            string[] fileName = Directory.GetFiles(directory, searchPattern, searchOption);
+
+            return fileName;
+        }
+        #endregion
+    }
+}
