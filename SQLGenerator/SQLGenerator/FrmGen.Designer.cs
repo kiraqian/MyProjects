144655251284bb45612d574b7e5dd3cc6dcc728e
lqian2 <light.qian@infor.com>
2018-03-07 10:10:59 +0800

    Add new project SQL Generator to source control.

---
+++ SQLGenerator/SQLGenerator/FrmGen.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace SQLGenerator
+{
+    partial class FrmGen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvConfig = new System.Windows.Forms.DataGridView();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtConnString = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvConfig)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvConfig
+            // 
+            this.dgvConfig.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvConfig.Location = new System.Drawing.Point(12, 32);
+            this.dgvConfig.Name = "dgvConfig";
+            this.dgvConfig.Size = new System.Drawing.Size(808, 350);
+            this.dgvConfig.TabIndex = 0;
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(745, 4);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(75, 23);
+            this.btnGenerate.TabIndex = 1;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Connection String:";
+            // 
+            // txtConnString
+            // 
+            this.txtConnString.Location = new System.Drawing.Point(112, 6);
+            this.txtConnString.Name = "txtConnString";
+            this.txtConnString.Size = new System.Drawing.Size(627, 20);
+            this.txtConnString.TabIndex = 3;
+            // 
+            // FrmGen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(832, 394);
+            this.Controls.Add(this.txtConnString);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.dgvConfig);
+            this.Name = "FrmGen";
+            this.Text = "SQL Generator";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FrmGen_FormClosing);
+            this.Load += new System.EventHandler(this.FrmGen_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvConfig)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvConfig;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtConnString;
+    }
+}
+
---
+++ SQLGenerator/SQLGenerator/FrmGen.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Utility.SqlUtil;
+using Utility.XmlUtil;
+using Utility.StringUtil;
+using Utility.FileUtil;
+
+namespace SQLGenerator
+{
+    public partial class FrmGen : Form
+    {
+        private DataTable dtCfg;
+        private string sqlConnString;
+
+        public FrmGen()
+        {
+            InitializeComponent();
+            dtCfg = new DataTable("ConfigTable");
+            dtCfg.Columns.Add("Selection");
+            dtCfg.Columns.Add("TemplateFile");
+            dtCfg.Columns.Add("OutputFile");
+            dgvConfig.DataSource = dtCfg;
+
+            try
+            {
+                dtCfg.ReadXml(Application.StartupPath + "\\CfgTable.xml");
+                txtConnString.Text = XmlUtility.ReadNodeValue(Application.StartupPath + "\\Settings.xml", "./Settings/ConnectionString");
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void FrmGen_Load(object sender, EventArgs e)
+        {
+            dgvConfig.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.DisplayedCells);
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                sqlConnString = txtConnString.Text;
+                GenSQLByRow(dtCfg, sqlConnString);
+                MessageBox.Show("All Completed!");
+            }
+            catch(Exception exp)
+            {
+                MessageBox.Show(exp.Message);
+            }
+        }
+
+        private void GenSQLByRow(DataTable configTable, string connString)
+        {
+            string selectionCmd;
+            string templateFile;
+            string outputFile;
+            string generatedSQLText = "";
+
+            DataTable dtTableData;
+
+            foreach(DataRow dr in configTable.Rows)
+            {
+                selectionCmd = dr["Selection"].ToString();
+                templateFile = dr["TemplateFile"].ToString();
+                outputFile = dr["OutputFile"].ToString();
+                
+                dtTableData = GetTableDataBySelection(connString, selectionCmd);
+                generatedSQLText = GetGeneratedSQLText(dtTableData, templateFile);
+                File.WriteAllText(outputFile, generatedSQLText);
+            }
+        }
+
+        private string GetGeneratedSQLText(DataTable dtTableData, string templateFile)
+        {
+            string columnName;
+            string cellData;
+            string templateFileContent = File.ReadAllText(templateFile);
+            string rowString = templateFileContent;
+            string resultString = "";            
+
+            string replacePrefix = "[###";
+            string replaceSuffix = "###]";
+
+            foreach (DataRow dr in dtTableData.Rows)
+            {
+                foreach (DataColumn dc in dtTableData.Columns)
+                {
+                    columnName = dc.ColumnName;
+                    if(Convert.IsDBNull(dr[columnName]))
+                    {
+                        cellData = "NULL";
+                        rowString = rowString.Replace("N'" + replacePrefix + columnName + replaceSuffix + "'", cellData);
+                        rowString = rowString.Replace("'" + replacePrefix + columnName + replaceSuffix + "'", cellData);
+                    }
+                    else
+                    {
+                        cellData = dr[columnName].ToString().Replace("'", "''");
+                        rowString = rowString.Replace(replacePrefix + columnName + replaceSuffix, cellData);
+                    }
+                }
+
+                resultString += rowString;
+                rowString = templateFileContent;
+            }
+            return resultString;
+        }
+
+        private DataTable GetTableDataBySelection(string connString, string selectionCmd)
+        {
+            string errMsg;
+            DataTable dtData = SqlUtility.QueryDBData(connString, selectionCmd, out errMsg);
+            return dtData;
+        }
+
+        private void FrmGen_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                dtCfg.WriteXml(Application.StartupPath + "\\CfgTable.xml");
+                XmlUtility.WriteNodeValue(Application.StartupPath + "\\Settings.xml", "./Settings/ConnectionString", txtConnString.Text);
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
