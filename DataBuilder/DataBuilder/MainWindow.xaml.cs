999c0eac28a402304289fe88ad6b2c3748dc6f19
lqian2 <light.qian@infor.com>
2021-04-25 15:45:15 +0800

    Fix a bug for trim end.

--- DataBuilder/DataBuilder/MainWindow.xaml.cs
+++ DataBuilder/DataBuilder/MainWindow.xaml.cs
@@ -1,18 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Data;
 using System.Xml;
 
@@ -110,8 +100,8 @@ namespace DataBuilder
             }
 
             string tableName = GetTableFromQuery(txtSelection.Text);
-            insertColumnList = insertColumnList.TrimEnd(' ', ',');
-            insertValueList = insertValueList.TrimEnd(' ', ',');
+            insertColumnList = insertColumnList.TrimEnd(' ', ',', '\r', '\n');
+            insertValueList = insertValueList.TrimEnd(' ', ',', '\r', '\n');
             string codeBlock = $"INSERT INTO {tableName} ({insertColumnList})\r\nVALUES({insertValueList})";
             txtCode.Text = codeBlock;
         }
