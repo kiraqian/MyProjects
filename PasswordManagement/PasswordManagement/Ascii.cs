b5bcd21537c127edea5cb4106a671b7695f8b7a5
lqian2 <light.qian@infor.com>
2021-04-26 14:45:27 +0800

    Rewrite encode-engine with WPF.

---
+++ PasswordManagement/PasswordManagement/Ascii.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace PasswordManagement
+{
+    public class Ascii
+    {
+        #region Constructor
+        public Ascii()
+        {
+        }
+        #endregion
+
+        #region Public Static Method
+        public static int GetAsciiCode(char character)
+        {
+            return Convert.ToInt32(character);
+        }
+
+        public static int[] GetMappingInt32(char[] charArray)
+        {
+            int[] retInt = new int[charArray.Length];
+            for (int i = 0; i < charArray.Length; i++)
+            {
+                retInt[i] = Ascii.GetAsciiCode(charArray[i]);
+            }
+            return retInt;
+        }
+
+        public static char AsciiToChar(int int32Value)
+        {
+            return Convert.ToChar(int32Value);
+        }
+
+        public static char[] AsciiToCharArray(int[] int32Array)
+        {
+            char[] retCharArray = new char[int32Array.Length];
+            for (int i = 0; i < int32Array.Length; i++)
+            {
+                retCharArray[i] = Ascii.AsciiToChar(int32Array[i]);
+            }
+            return retCharArray;
+        }
+        #endregion
+    }
+}
---
+++ PasswordManagement/PasswordManagement/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Windows;
+
+namespace PasswordManagement
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Core _core;
+        public MainWindow()
+        {
+            InitializeComponent();
+            _core = new Core();
+        }
+
+        private void btnEncode_Click(object sender, RoutedEventArgs e)
+        {
+            if(txtPassword.Password != string.Empty &&
+                txtPassword.Password == txtPasswordConfirm.Password)
+            {
+                try
+                {
+                    _core.CustomEncodeText = txtPassword.Password;
+                    _core.OriginalText = txtContent.Text;
+                    txtContent.Text = _core.Encode();
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Password not matched!");
+            }
+        }
+
+        private void btnDecode_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtPassword.Password != string.Empty &&
+                txtPassword.Password == txtPasswordConfirm.Password)
+            {
+                try
+                {
+                    _core.CustomEncodeText = txtPassword.Password;
+                    _core.OriginalText = txtContent.Text;
+                    txtContent.Text = _core.Decode();
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Password not matched!");
+            }
+        }
+    }
+}
